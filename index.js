bd6b93a8754c8b187b9531f219e21ed03ab88dca
square <artubr@gmail.com>
2020-12-28 01:14:36 +0100

    style change

--- index.js
+++ index.js
@@ -178,4 +178,4 @@ app.post('/subscribe', (req, res) => {
       .catch(error => console.error(error));
 });
 
-app.listen(80, () => console.log('Listening on port 80'));
+app.listen(80, () => console.log('Listening on Port 80'));
